cd503a5d2110588e7938297d04a74138e0804f95
Ray <ray@example.com>
2023-03-11 15:44:01 +0000

    add hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
+1+2
 print("hello!")
 print("world")
 print("12342")
@@ -8,3 +10,6 @@ print("5+5=", 5+5)
 print("5+5=", 5+5)
 print("5+5=", 5+5)
 print("5+5=", 5+5)
+
+plt.plot([1,2,3],[4,5,6])
+plt.show()
